ed5e66054a297785d221c2c47b7cc368e04faa7d
Hans Dushanthakumar <hans@thoughtworks.com>
2014-09-28 17:58:23 +1000

    CI Agent config now works

---
+++ 03_ContinuousIntegration/CI_Agent/metadata.rb
@@ -0,0 +1,7 @@
+name             'CI_Agent'
+maintainer       'Hans'
+maintainer_email 'hans@thoughtworks.com'
+license          'All rights reserved'
+description      'Installs & configures VMs for the CI workshop'
+long_description 'Installs & configures VMs for the CI workshop'
+version          '0.1.0'
