863294dae0ffc2cc1440885d3f0ca93ed7f49e8c
lnkhai <lnkhai@gmail.com>
2014-10-16 20:05:44 +1100

    Update webserver.rb

--- 02_ProvisioningTestEnvironment/machines/recipes/webserver.rb
+++ 02_ProvisioningTestEnvironment/machines/recipes/webserver.rb
@@ -1,1 +1,13 @@
-# include_recipe 'apache2'
+service 'apache2' do
+	init_command '/etc/init.d/apache2'
+	action :restart
+end
+
+directory "/var/www/sydneytester" do
+  action :create
+end
+
+file '/var/www/sydneytester/index.php' do
+	content "<?php echo '<h1>Hello SydneyTester</h1>'; ?>"
+	action :create
+end
